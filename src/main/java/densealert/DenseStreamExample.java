3cf8d393bd818b8e1ad108c150c07f6c0795a315
sachaStewart <sachalevy26@gmail.com>
2020-04-13 00:22:17 -0400

    added proper densestream file

--- src/main/java/densealert/DenseStreamExample.java
+++ src/main/java/densealert/DenseStreamExample.java
@@ -19,6 +19,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.io.*; 
+import java.util.List;
+
+import java.io.FileWriter;
 
 /**
  * Example Using DenseStream
@@ -36,13 +42,13 @@ public class DenseStreamExample {
         /**
          * Initialize DenseStream Object
          */
+        //final String dataPath = "../coupled/tensors/denseAlert_example.txt";
         final String dataPath = "example_data.txt";
+        final String output_file = "denseBlocks_output.txt";
         final int order = 3;
         final String delim = ",";
-        final DenseStream denseStream = new DenseStream(order);
-
-
-
+        final DenseStream denseStream = new DenseStream(order);        
+        String density_key = "'density'";
         /**
          * insert entries one by one
          */
@@ -61,7 +67,33 @@ public class DenseStreamExample {
             denseStream.insert(parseEntry(line, delim, order));
 
             if((++count) % 100000 == 0) {
+                
                 System.out.println("Processed Entry Num: " + count + ", Current Density: " + denseStream.getDensity());
+                Map<Integer, int[]> modeToIndices= denseStream.getBlockIndices();
+                // will hold the current dense block values for indexes & modes
+                Map<String, String> block = new HashMap<String, String>();
+
+                /**
+                 * Print the intermediate maintained Block
+                 */
+                System.out.println("Indices forming a dense block");
+                for(int mode = 0; mode < order; mode++) {
+                    String ids = "[";
+                    System.out.println("Mode " + mode +": ");
+                    int[] indices = modeToIndices.get(mode);
+
+                    for(int i=0; i<indices.length; i++) {
+                        System.out.print(indices[i]+" ");
+                        ids += String.valueOf(indices[i])+",";
+                    }
+                    ids += "]";
+                    block.put('"'+String.valueOf(mode)+'"', ids);
+                }
+                block.put(density_key.toString(), String.valueOf(denseStream.getDensity()));
+                // write updated block
+                BufferedWriter writer = new BufferedWriter( new FileWriter(output_file, true)); 
+                writer.write(block.toString().replace("=", ":").replace(",]", "]")+"\n");
+                writer.close();
             }
         }
 
